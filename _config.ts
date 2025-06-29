f2cd8e92b681f20b0a892ecd2490190a573b4991
Natouche68 <ngrossheny@gmail.com>
2025-06-29 19:48:53 +0200

    Add Tailwind CSS

--- _config.ts
+++ _config.ts
@@ -1,7 +1,9 @@
 import lume from "lume/mod.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
 
 const site = lume();
 
-site.add("static")
+site.use(tailwindcss());
+site.add("static");
 
 export default site;
