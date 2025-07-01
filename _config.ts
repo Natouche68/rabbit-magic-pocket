b2cf18d2186c52aa5f9a2ac4746fb748b0d605fb
Natouche68 <ngrossheny@gmail.com>
2025-07-01 12:29:56 +0200

    Add an icon

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
+import favicon from "lume/plugins/favicon.ts";
 import { alert } from "npm:@mdit/plugin-alert";
 
 const site = lume();
@@ -7,6 +8,8 @@ const site = lume();
 site.hooks.addMarkdownItPlugin(alert, {
 	alertNames: ["important", "note", "tip", "warning", "caution", "setup"],
 });
+
+site.use(favicon());
 
 site.use(tailwindcss());
 site.add("static");
